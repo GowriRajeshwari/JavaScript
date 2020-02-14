8b3975055a303d48f56ae5d76714f12be6ad6567
GowriRajeshwari <gowrikanaga216@gmail.com>
2020-02-14 12:05:14 +0530

    [GowriK]. Refactor write an recursive method,when user give the input less than 2 characters,it will ask again to enter your name

--- JavaScript/BasicProgram/ReplaceString/ReplaceStringBL.js
+++ JavaScript/BasicProgram/ReplaceString/ReplaceStringBL.js
@@ -1,12 +1,17 @@
+const readline = require('readline-sync');
 module.exports={
     replacestring:(name)=>{
         if(name.length > 3){
             let string="Hello <<UserName>>, How are you?";
-            var res=string.replace("<<UserName>>",name);
-            return res;
+            let res=string.replace("<<UserName>>",name);
+            // console.log(res);
+             return res;
         }
         else{
-            return "Name should max of three character's";
+            console.log("Name should max of three character's");
+            let name=readline.question("Enter your Name : ");
+            let ans=module.exports.replacestring(name);
+            return ans;
             
         }
     }
