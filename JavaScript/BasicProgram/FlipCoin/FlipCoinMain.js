6535c4798859ffad9fd63e28591b5c00436804e5
GowriRajeshwari <gowrikanaga216@gmail.com>
2020-02-14 12:26:08 +0530

    [GowriK]. Refactor : changed the input datatype from string to integer

--- JavaScript/BasicProgram/FlipCoin/FlipCoinMain.js
+++ JavaScript/BasicProgram/FlipCoin/FlipCoinMain.js
@@ -1,7 +1,7 @@
 const readline = require('readline-sync');
 const callfunction = require('./FlipCoinBL');
 try{
-    let no=readline.question("Enter No of times the coin want to flip : ");
+    let no=readline.questionInt("Enter No of times the coin want to flip : ");
     let ans=callfunction.flipcoin(no);
     console.log("Percentage of head : "+ans.headpercent+"%"+" , Percentage of tail : ",ans.tailpercent+"%");
 }
