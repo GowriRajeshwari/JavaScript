0f2352aa4b62daa1ee342d06ccb0171dbdd9be82
GowriRajeshwari <gowrikanaga216@gmail.com>
2020-02-14 11:40:14 +0530

    [GowriK]. Ensuring it is an 4 digit No,and write an logic to check for an leap year or not

---
+++ JavaScript/BasicProgram/LeapYear/LeapYearBL.js
@@ -0,0 +1,16 @@
+module.exports={
+    leapyear:(year)=>{
+        if(year.length >= 4){
+            if( (year % 4 == 0 ) && (year % 100 == 0) || (year % 400 == 0) ){
+                return "Given Year is the Leap Year";
+            }
+            else{
+             return "Given Year is Not an Leap Year";
+            }
+        }
+        else{
+            return "Ensure whether it is 4 digit no";
+        }
+       
+    }
+}
