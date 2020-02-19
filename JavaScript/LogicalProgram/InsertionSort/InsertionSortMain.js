6e62653aa0831cf3b825fb664aed7efa908f73c7
GowriRajeshwari <gowrikanaga216@gmail.com>
2020-02-19 10:58:19 +0530

    [GowriK]. Removed the folder from repository

--- JavaScript/LogicalProgram/InsertionSort/InsertionSortMain.js
+++
@@ -1,9 +0,0 @@
-const readline = require('readline-sync');
-const callfunction = require('./InsertionSortBL');
-try{
-    let ans=callfunction.insertionsort();
-    console.log(ans);
-}
-catch(error){
-    console.log(error);
-}
