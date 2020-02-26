fe3b8c2527e4efc6d45318c173863206f208c411
GowriRajeshwari <gowrikanaga216@gmail.com>
2020-02-26 11:55:38 +0530

    [GowriK]. Refactor : replace string Bussiness logic

--- Testing/mochatest/BL/ReplaceStringBL.js
+++ Testing/mochatest/BL/ReplaceStringBL.js
@@ -1,13 +1,19 @@
 const readline = require('readline-sync');
 module.exports={
     replacestring:(name)=>{
+        if(isNaN(name)){
+        
         let string="Hello <<UserName>>, How are you?";
         if(name.length > 3){
             let res=string.replace("<<UserName>>",name);
             // console.log(res);
-             return res;
+             return name;
         }
         return string;
+    }
+    else{
+        return undefined;
+    }
         
     }
 }
