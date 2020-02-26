02e5f9e0300bbe35d251d30e8d7e6500631aa8eb
GowriRajeshwari <gowrikanaga216@gmail.com>
2020-02-26 12:38:14 +0530

    [GowriK]. Refactor : modfied the code according to the test case

--- Testing/mochatest/BL/FlipCoinBL.js
+++ Testing/mochatest/BL/FlipCoinBL.js
@@ -1,21 +1,32 @@
-module.exports={
-    flipcoin:(no)=>{
-        let head=0,tail=0;
-        for(let i=0;i<no;i++ ){
-            let value = Math.random();
-            if(value < 0.5 ){
-                head++;
+module.exports = {
+    flipcoin: (no) => {
+        if (isNaN(no) || Math.floor(no) !== no) {
+            return undefined;
+        }
+        else {
+            if (no > 0) {
+                let head = 0, tail = 0;
+                for (let i = 0; i < no; i++) {
+                    let value = Math.random();
+                    if (value < 0.5) {
+                        head++;
+                    }
+                    else {
+                        tail++;
+                    }
+                }
+                let headpercent = head / no * 100;
+                let tailpercent = tail / no * 100;
+                return {
+                    headpercent: headpercent,
+                    tailpercent: tailpercent
+                };
             }
             else{
-                tail++;
+                return undefined;
             }
+    
+
         }
-        let headpercent = head/no*100;
-        let tailpercent = tail/no*100;
-        return{
-            headpercent:headpercent,
-            tailpercent:tailpercent
-        };
-       
     }
 }
