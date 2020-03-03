9d3cb30e9f7befea678275bdd9035e7f0afa5d81
parmeshwarraut1996 <parmeshwarraut1996@gmail.com>
2020-03-03 19:04:46 +0530

    [GowriK] Deleted the folder in respoistory

--- JavaScript/BasicProgram/Powerof2/Powerof2Main.js
+++
@@ -1,9 +0,0 @@
-const readline = require('readline-sync');
-const callfunction = require('./Powerof2BL');
-try{
-    
-    let ans=callfunction.powerof2(process.argv[2]);
-}
-catch(error){
-    console.log(error);
-}
